bdd68c23fb2706417bf365996d655796a52b89d0
unknown <swapnil.sawant.me@gmail.com>
2024-01-08 14:33:56 +0530

    Model Trainer Added

--- src/mlProject/pipeline/stage_03_data_transformation.py
+++ src/mlProject/pipeline/stage_03_data_transformation.py
@@ -1,7 +1,7 @@
 from mlProject.config.configuration import ConfigurationManager
 from mlProject.components.data_transformation import DataTransformation
 from mlProject import logger
-from pathlib import Path
+from pathlib import Path 
 
 
 
